8887198e0243af9d8d645d41faea939a08ca30bb
matepaic <mate.paic1997@gmail.com>
2022-09-07 18:13:13 +0200

    End of Section 2 Created new WebApi. Implemented Domain and injected DBContext Class

---
+++ CroatiaWalks/CroatiaWalks.API/Data/CROWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CroatiaWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CroatiaWalks.API.Data
+{
+    public class CROWalksDbContext:DbContext
+    {
+        public CROWalksDbContext(DbContextOptions<CROWalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
