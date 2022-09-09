e5c1adcc3a108568e4f91351af5b698be942ae91
matepaic <mate.paic1997@gmail.com>
2022-09-09 10:59:48 +0200

    End of Section 4 Created GetById, Posting, Updating and Deleting Region

--- CroatiaWalks/CroatiaWalks.API/Controllers/RegionsController.cs
+++ CroatiaWalks/CroatiaWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CroatiaWalks.API.Models.Domain;
+using CroatiaWalks.API.Models.DTO;
 using CroatiaWalks.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -67,6 +68,122 @@ namespace CroatiaWalks.API.Controllers
             var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
 
             return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+
+            return Ok(regionDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
+        {
+            //Request(DTO) to Domain Model
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Name = addRegionRequest.Name,
+                Population = addRegionRequest.Population
+            };
+            //Pass details to Repository
+            region = await regionRepository.AddAsync(region);
+
+            //Convert back to DTO
+            var regionDTO = new Models.DTO.Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Name = region.Name,
+                Population = region.Population
+            };
+
+            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDTO.Id}, regionDTO); //201
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            //Get region from database
+            var deletedRegion = await regionRepository.DeleteAsync(id);
+
+            //If null NotFound
+            if (deletedRegion == null)
+            {
+                return NotFound();
+            }
+
+            //Convert response back to DTO
+            var deletedRegionDTO = new Models.DTO.Region
+            {
+                Id = deletedRegion.Id,
+                Code = deletedRegion.Code,
+                Area = deletedRegion.Area,
+                Lat = deletedRegion.Lat,
+                Long = deletedRegion.Long,
+                Name = deletedRegion.Name,
+                Population = deletedRegion.Population
+            };
+
+            //return Ok response
+            return Ok(deletedRegionDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute] Guid id, [FromBody] Models.DTO.UpdateRegionRequest updateRegionRequest)
+        {
+            //Convert DTO to Domain model
+            var updatedRegion = new Models.Domain.Region()
+            {
+                Code = updateRegionRequest.Code,
+                Area = updateRegionRequest.Area,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Name = updateRegionRequest.Name,
+                Population = updateRegionRequest.Population
+            };
+
+            //Update Region using repositroy
+            updatedRegion = await regionRepository.UpdateAsync(id, updatedRegion);
+
+            //If Null then NotFound
+            if (updatedRegion == null)
+            {
+                return NotFound();
+            }
+
+            //Convert Domain back to DTO
+            var updatedRegionDTO = new Models.DTO.Region
+            {
+                Id = updatedRegion.Id,
+                Code = updatedRegion.Code,
+                Area = updatedRegion.Area,
+                Lat = updatedRegion.Lat,
+                Long = updatedRegion.Long,
+                Name = updatedRegion.Name,
+                Population = updatedRegion.Population
+            };
+
+            //Return Ok reponse
+            return Ok(updatedRegionDTO);
         }
     }
 }
--- CroatiaWalks/CroatiaWalks.API/Repositories/RegionRepository.cs
+++ CroatiaWalks/CroatiaWalks.API/Repositories/RegionRepository.cs
@@ -17,5 +17,52 @@ namespace CroatiaWalks.API.Repositories
         {
             return await cROWalksDbContext.Regions.ToListAsync();
         }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await cROWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await cROWalksDbContext.AddAsync(region);
+            await cROWalksDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await cROWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (region == null)
+            {
+                return null;
+            }
+
+            //Delete the Region
+            cROWalksDbContext.Regions.Remove(region);
+            await cROWalksDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await cROWalksDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingRegion == null)
+            {
+                return null;
+            }
+
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.Area = region.Area;
+            existingRegion.Lat = region.Lat;
+            existingRegion.Long = region.Long;
+            existingRegion.Population = region.Population;
+
+            await cROWalksDbContext.SaveChangesAsync();
+
+            return existingRegion;
+        }
     }
 }
